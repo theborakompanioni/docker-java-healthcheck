d6ebf5736377cbe0e7f9ed47011be269ba6de08d
theborakompanioni <theborakompanioni+github@gmail.com>
2025-02-20 11:42:55 +0100

    chore: try-with-resource

--- HealthCheck.java
+++ HealthCheck.java
@@ -1,28 +1,23 @@
-import java.io.IOException;
-
 import java.net.URI;
-
 import java.net.http.HttpClient;
-
 import java.net.http.HttpRequest;
-
 import java.net.http.HttpResponse.BodyHandlers;
 
 public class HealthCheck {
+    public static void main(String[] args) {
+        try (var client = HttpClient.newHttpClient()) {
+            var request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:8080/actuator/health"))
+                    .header("accept", "application/json")
+                    .build();
 
-  public static void main(String[] args) throws InterruptedException, IOException {
-    var client = HttpClient.newHttpClient();
+            var response = client.send(request, BodyHandlers.ofString());
 
-    var request = HttpRequest.newBuilder()
-        .uri(URI.create("http://localhost:8080/actuator/health"))
-        .header("accept", "application/json")
-        .build();
-
-    var response = client.send(request, BodyHandlers.ofString());
-
-    if (response.statusCode() != 200 || !response.body().contains("UP")) {
-      throw new RuntimeException("Healthcheck failed");
+            if (response.statusCode() != 200 || !response.body().contains("UP")) {
+                throw new RuntimeException("Healthcheck failed");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
-  }
-
 }
