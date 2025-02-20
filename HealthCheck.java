e3dd01d9fbd66cf5df97119b95ef6881d1759fc7
theborakompanioni <theborakompanioni+github@gmail.com>
2025-02-20 12:17:00 +0100

    chore: url param

--- HealthCheck.java
+++ HealthCheck.java
@@ -1,4 +1,3 @@
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -6,29 +5,33 @@ import java.net.http.HttpResponse.BodyHandlers;
 
 public class HealthCheck {
     public static void main(String[] args) {
-        int port = Integer.parseInt(args.length >= 1 ? args[0] : "8080");
-        String path = args.length >= 2 ? args[1] : "/actuator/health";
-        String line = args.length >= 3 ? args[2] : "\"status\":\"UP\"";
-        String protocol = args.length >= 4 ? ("https".equals(args[3]) ? "https" : "http") : "http";
-        int statusCode = Integer.parseInt(args.length >= 5 ? args[4] : "200");
+        try {
+            String url = args.length >= 1 ? args[0] : "http://localhost:8080/actuator/health";
+            String line = args.length >= 2 ? args[1] : "\"status\":\"UP\"";
+            int statusCode = Integer.parseInt(args.length >= 3 ? args[2] : "200");
 
-        URI uri = URI.create("%s://localhost:%d%s".formatted(protocol, port, path));
-        try (var client = HttpClient.newHttpClient()) {
-            var request = HttpRequest.newBuilder()
-                    .uri(uri)
-                    .header("accept", "application/json")
-                    .build();
-
-            var response = client.send(request, BodyHandlers.ofString());
-
-            if (response.statusCode() != statusCode) {
-                System.err.printf("Healthcheck error: Mismatching status code. Expected `%d`, was `%d`.%n",
-                        statusCode, response.statusCode());
-                System.exit(2);
+            URI uri = URI.create(url);
+            if (!"localhost".equals(uri.getHost())) {
+                throw new IllegalArgumentException("Healthcheck only works with `localhost`.");
             }
-            if (response.body().lines().noneMatch(it -> it.contains(line))) {
-                System.err.printf("Healthcheck error: Body does not contains line `%s`.%n", line);
-                System.exit(3);
+
+            try (var client = HttpClient.newHttpClient()) {
+                var request = HttpRequest.newBuilder()
+                        .uri(uri)
+                        .header("accept", "application/json")
+                        .build();
+
+                var response = client.send(request, BodyHandlers.ofString());
+
+                if (response.statusCode() != statusCode) {
+                    System.err.printf("Healthcheck error: Mismatching status code. Expected `%d`, was `%d`.%n",
+                            statusCode, response.statusCode());
+                    System.exit(2);
+                }
+                if (response.body().lines().noneMatch(it -> it.contains(line))) {
+                    System.err.printf("Healthcheck error: Body does not contains line `%s`.%n", line);
+                    System.exit(3);
+                }
             }
 
             System.out.printf("Healthcheck success%n");
