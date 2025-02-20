3a1b750118387dcc6a1e88bd3b851b573c94c2ee
theborakompanioni <theborakompanioni+github@gmail.com>
2025-02-20 12:09:19 +0100

    chore: status code and protocol

--- HealthCheck.java
+++ HealthCheck.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -8,21 +9,33 @@ public class HealthCheck {
         int port = Integer.parseInt(args.length >= 1 ? args[0] : "8080");
         String path = args.length >= 2 ? args[1] : "/actuator/health";
         String line = args.length >= 3 ? args[2] : "\"status\":\"UP\"";
-        String protocol = args.length >= 3 ? ("https".equals(args[3]) ? "https" : "http") : "http";
+        String protocol = args.length >= 4 ? ("https".equals(args[3]) ? "https" : "http") : "http";
+        int statusCode = Integer.parseInt(args.length >= 5 ? args[4] : "200");
 
+        URI uri = URI.create("%s://localhost:%d%s".formatted(protocol, port, path));
         try (var client = HttpClient.newHttpClient()) {
             var request = HttpRequest.newBuilder()
-                    .uri(URI.create("%s://localhost:%d%s".formatted(protocol, port, path)))
+                    .uri(uri)
                     .header("accept", "application/json")
                     .build();
 
-            var response = client.send(request, BodyHandlers.ofLines());
+            var response = client.send(request, BodyHandlers.ofString());
 
-            if (response.statusCode() != 200 || response.body().noneMatch(it -> it.contains(line))) {
-                throw new RuntimeException("Healthcheck failed");
+            if (response.statusCode() != statusCode) {
+                System.err.printf("Healthcheck error: Mismatching status code. Expected `%d`, was `%d`.%n",
+                        statusCode, response.statusCode());
+                System.exit(2);
             }
+            if (response.body().lines().noneMatch(it -> it.contains(line))) {
+                System.err.printf("Healthcheck error: Body does not contains line `%s`.%n", line);
+                System.exit(3);
+            }
+
+            System.out.printf("Healthcheck success%n");
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            System.err.printf("Error: %s%n", e.getClass().getSimpleName());
+            e.printStackTrace(System.err);
+            System.exit(1);
         }
     }
 }
